24612eeed9e920f37924504415a083daeda2cba5
Dionis <dionisenja@gmail.com>
2021-12-21 12:32:54 +0100

    User input, arithmetics and math class

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*User Input
+Console.WriteLine("What's your name?");
+String name = Console.ReadLine();
+Console.WriteLine("What's your age?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Hello {name}");
+Console.WriteLine($"Your current age is {age}");
+Console.WriteLine($"Thank you {name} you can GTFO now! :)");
+*/
+
+/*Arithmetics
+Console.WriteLine("What's your number of friends?");
+int friends = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Hi stranger you currently have {friends} friends");
+friends += 5;
+friends -= 3;
+friends *= 2;
+friends /= 4;
+Console.WriteLine($"Hi stranger after a while you currently have {friends} friends");
+*/
+
+/*Math Class
+double x = 5;
+double y = 3;
+double a = Math.Pow(x, 2);
+Console.WriteLine(a);
+double b = Math.Sqrt(x);
+Console.WriteLine(b);
+double c = Math.Abs(x);
+Console.WriteLine(c);
+double d = Math.Max(x, y);
+Console.WriteLine(d);
+double e = Math.Min(x, y);
+Console.WriteLine(e);
+*/
+
+
+
+
