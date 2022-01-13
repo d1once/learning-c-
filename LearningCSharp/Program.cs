dcac2a1799a9bcd271b47cc0cbb8533ed11722b9
Dionis <dionisenja@gmail.com>
2022-01-13 14:42:33 +0100

    Params keyword

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -435,6 +435,26 @@ static double Multiply(double a, double b, double c)
 }
 */
 
+/*Params keyword
+//params keyword = a method parameter that takes a variable number of arguments.
+//                 The parameter type must be a single - dimensional array
+
+double total = CheckOut(3.99, 5.75, 15, 1.00, 10.25);
+
+Console.WriteLine(total);
+
+static double CheckOut(params double[] prices)
+{
+    double total = 0;
+
+    foreach (double price in prices)
+    {
+        total += price;
+    }
+    return total;
+}
+*/
+
 
 
 
