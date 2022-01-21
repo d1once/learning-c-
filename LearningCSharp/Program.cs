f5bd32f27f001e2fff32b1a4e39d7c8c9a56d890
Dionis <dionisenja@gmail.com>
2022-01-21 15:28:46 +0100

    classes

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -522,5 +522,22 @@ foreach(String car in parkingLot) {
 }
 */
 
+/* Classes 
 
+ static class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to the program");
+        }
+        public static void Waiting()
+        {
+            Console.WriteLine("I am waiting for something");
+        }
+        public static void Bye()
+        {
+            Console.WriteLine("Bye! Thanks for visiting");
+        }
+    }
+*/
 
