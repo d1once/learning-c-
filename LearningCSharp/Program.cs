3c4f9b900ec37543539d0f8d40c4d6118ac6e840
dionce <dionisenja@gmail.com>
2022-01-22 20:26:24 +0100

    objects

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -541,3 +541,35 @@ foreach(String car in parkingLot) {
     }
 */
 
+/*Objects
+            Human human1 = new Human();
+            Human human2 = new Human();
+
+            human1.name = "Rick";
+            human1.age = 65;
+
+            human2.name = "Morty";
+            human2.age = 16;
+
+            human1.Eat();
+            human1.Sleep();
+
+            human2.Eat();
+            human2.Sleep();
+
+            Console.ReadKey();
+    class Human
+    {
+        public String name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine(name + " is eating");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping");
+        }
+    }
+*/
