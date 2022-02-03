c5575b16f2881f298508d799ce55fe4eb56b1ff6
Dionis <dionisenja@gmail.com>
2022-02-03 14:38:08 +0100

    object as arguments

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -778,3 +778,36 @@ class Car
   }
 }
 */
+
+/*Objects as arguments
+  Car car1 = new Car("Mustang","red");
+
+            Car car2 = Copy(car1);
+
+            Console.WriteLine(car2.color + " " + car2.model);
+
+            Console.ReadKey();
+        
+
+        public static void ChangeColor(Car car, String color)
+        {
+            car.color = color;
+        }
+
+        public static Car Copy(Car car)
+        {
+            return new Car(car.model, car.color);
+        }
+    
+    class Car 
+    {
+        public String model;
+        public String color;
+
+        public Car(String model, String color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+*/
