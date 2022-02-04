46e72950b2b12c44681579ac2f440e4de9dfd817
Dionis <dionisenja@gmail.com>
2022-02-04 13:42:50 +0100

    method override

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -811,3 +811,35 @@ class Car
         }
     }
 */
+
+/*Method overriding
+    //method overriding = provides a new version of a method inherited from a parent class
+            //                    inherited method must be: abstract, virtual, or already overriden
+            //                    Used with ToString(), polymorphism 
+
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.Speak();
+            cat.Speak();
+
+            Console.ReadKey();
+    class Animal 
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The animal goes *brrr*");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The dog goes *woof*");
+        }
+    }
+    class Cat : Animal
+    {
+
+    }
+*/
