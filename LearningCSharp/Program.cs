82f4a9bee3ea141ba383a4b22c29a7b18e564699
dionce <dionisenja@gmail.com>
2022-02-05 23:37:17 +0100

    to string method

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -843,3 +843,31 @@ class Car
 
     }
 */
+
+/*To string method
+Car car = new Car("Chevy", "Corvette", 2022, "blue");
+
+Console.WriteLine(car.ToString());
+
+Console.ReadKey();
+
+class Car
+{
+  String make;
+  String model;
+  int year;
+  String color;
+
+  public Car(String make, String model, int year, String color)
+  {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+  }
+  public override string ToString()
+  {
+    return "This is a " + make + " " + model;
+  }
+}
+*/
