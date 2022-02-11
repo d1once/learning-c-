92240b904bb1d850836148c0db9f5eff4c74d92e
dionce <dionisenja@gmail.com>
2022-02-11 21:50:20 +0100

    stters and getters

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -1009,3 +1009,14 @@ class Player {
     }
 }
 */
+
+/*Getter and setters
+
+     //getters & setters = add security to fields by encapsulation
+            //                    They're accessors found within properties
+
+            // properties = combine aspects of both fields and methods (share name with a field)
+            // get accessor = used to return the property value
+            // set accessor = used to assign a new value
+            // value keyword = defines the value being assigned by the set (parameter)
+*/
